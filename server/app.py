e324f6075e910224968ca2d929f95fcf06bbd808
Admin <ronghenidhi@gmail.com>
2026-04-12 15:12:11 +0530

    Fix openenv validate

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from api.main import app
 import uvicorn
 
 def main():
-    uvicorn.run("api.main:app", host="0.0.0.0", port=8000)
+    uvicorn.run("api.main:app", host="0.0.0.0", port=7860)
 
 if __name__ == "__main__":
     main()
